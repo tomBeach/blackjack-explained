59d02cf2d7487e27c8f09c004fccb19b64d58c3b
tomBeach <tbeach2k@gmail.com>
2015-12-02 10:46:16 -0500

    improved info text; scrolling issue inprocess

--- script.js
+++ script.js
@@ -7,23 +7,35 @@ function init() {
     var prevIndex = 0;
     var nextIndex = 0;
     var scrollStart = 0;
+    var label1startY = 0;
+    var label2startY = 0;
     var prevTextTrigger = 0;
     var nextTextTrigger = 60;
-    var gridLoc = $("#gridWindow").offset();
+    var gridLoc = $("#gridWindow").position();
     var gridX = gridLoc.left;
     var gridY = gridLoc.top;
-    console.log("  gridX: " + gridX);
-    console.log("  gridY: " + gridY);
+    // console.log("  gridX: " + gridX);
+    // console.log("  gridY: " + gridY);
     var info = $("#info");
     var gridImage = $("#gridWindow");
     var sectionTitle = $(".section-title");
     var scrollTop, infoTextArray, labelTextArray;
 
-    // == window scroll event listener
+    // == display info text with info scroll
     $("#scrollWindow").scrollTop(scrollStart);
     $("#scrollWindow").on('scroll', function(event) {
         updateGridText();
     });
+
+    // == update text loc if window scrolls
+    $(window).on('scroll', function(event) {
+        updateGridTextLoc();
+    });
+    // jQuery(window).bind('scrollstop', function(e){
+    //     updateLabelStarts();
+    // });
+
+    // == toggle scroll tooltips (avoid annoying user with help)
     $("#scrollWindow").on('mouseover', function(event) {
         updateInfoText();
     });
@@ -62,37 +74,37 @@ function init() {
             text1: { text: "merged", X:230, Y:150 },
             text2: { text: "unmerged", X:230, Y:270 }
         },
-        { image: "grid5", triggerNext:730,
+        { image: "grid5", triggerNext:720,
             section: "Spans",
             text1: { text: "colspan", X:230, Y:150 },
             text2: { text: "rowspan", X:150, Y:280 }
         },
-        { image: "colspan1", triggerNext:800,
+        { image: "colspan1", triggerNext:830,
             section: "Spans",
             text1: { text: "colspan (3 cells)", X:90, Y:150 },
             text2: { text: "target location (col 13)", X:350, Y:150 }
         },
-        { image: "colspan2", triggerNext:920,
+        { image: "colspan2", triggerNext:850,
             section: "Spans",
             text1: { text: "colspan (3 cells)", X:90, Y:150 },
             text2: { text: "actual location (col 10)", X:270, Y:150 }
         },
-        { image: "colspan2", triggerNext:960,
+        { image: "colspan2", triggerNext:940,
             section: "Spans",
             text1: { text: "colspan (3 cells)", X:90, Y:150 },
             text2: { text: "'missing' cells if uncorrected", X:400, Y:230 }
         },
-        { image: "rowspans7a", triggerNext:1000,
+        { image: "rowspans7a", triggerNext:980,
             section: "Spans",
             text1: { text: "region with rowspans", X:120, Y:150 },
             text2: { text: "spanned cells", X:150, Y:230 }
         },
-        { image: "rowspans8", triggerNext:1100,
+        { image: "rowspans8", triggerNext:1050,
             section: "Spans",
             text1: { text: "spanned cells", X:150, Y:230 },
             text2: { text: "un-affected index cell", X:320, Y:150 }
         },
-        { image: "rowspans9", triggerNext:1200,
+        { image: "rowspans9", triggerNext:1180,
             section: "Spans",
             text1: { text: "spanned cells", X:150, Y:230 },
             text2: { text: "affected index cell", X:340, Y:280 }
@@ -104,17 +116,17 @@ function init() {
         },
         { image: "rowspans11", triggerNext:1400,
             section: "Rowspans",
-            text1: { text: "initialized false", X:150, Y:230 },
-            text2: { text: "{r:5, c:5, rspan:f}", X:200, Y:150 }
+            text1: { text: "initialized false", X:150, Y:190 },
+            text2: { text: "{r:5, c:4, rspan:f}", X:150, Y:250 }
         },
-        { image: "rowspans12", triggerNext:1600,
+        { image: "rowspans12", triggerNext:1580,
             section: "Rowspans",
-            text1: { text: "rowspanned cells", X:150, Y:230 },
-            text2: { text: "{r:5, c:5, rspan:t}", X:200, Y:150 }
+            text1: { text: "rowspanned cells", X:150, Y:220 },
+            text2: { text: "{r:5, c:5, rspan:t}", X:150, Y:250 }
         },
         { image: "rowspans13", triggerNext:1700,
             section: "Rowspans",
-            text1: { text: "target rows", X:200, Y:230 },
+            text1: { text: "target row", X:200, Y:250 },
             text2: null
         },
         { image: "rowspans14", triggerNext:1900,
@@ -122,17 +134,38 @@ function init() {
             text1: { text: "merged region", X:150, Y:150 },
             text2: null
         }
-    ]
+    ];
 
-        // ======= ======= ======= updateInfoText ======= ======= =======
-        function updateInfoText() {
-            console.log("updateInfoText");
+    // ======= ======= ======= updateLabelStarts ======= ======= =======
+    function updateLabelStarts() {
+        console.log("updateLabelStarts");
 
-            if (infoFlag == false) {
-                $(info).html("scroll <span class='hilite'>slowly</span> for descriptions...");
-                infoFlag = true;
-            }
+        // label1startY = $('#labelText1').css('top');
+        // label2startY = $('#labelText2').css('top');
+    }
+
+    // ======= ======= ======= updateGridTextLoc ======= ======= =======
+    function updateGridTextLoc() {
+        console.log("updateGridTextLoc");
+
+        var $window = $(window);
+        var windowScrollTop = $window.scrollTop();
+        label1locY = label1startY - windowScrollTop;
+        label2locY = label2startY - windowScrollTop;
+        $('#labelText1').css('top', label1locY);
+        $('#labelText2').css('top', label2locY);
+
+    }
+
+    // ======= ======= ======= updateInfoText ======= ======= =======
+    function updateInfoText() {
+        console.log("updateInfoText");
+
+        if (infoFlag == false) {
+            $(info).html("scroll <span class='hilite'>slowly</span> for descriptions...");
+            infoFlag = true;
         }
+    }
 
     // ======= ======= ======= updateGridText ======= ======= =======
     function updateGridText() {
@@ -182,22 +215,28 @@ function init() {
     function updateLabels(gridData) {
         console.log("updateLabels");
 
+        // var gridLoc = $("#gridWindow").position();
+        // var gridX = gridLoc.left;
+        // var gridY = gridLoc.top;
+
         var whichLabel1 = gridData.text1;
         var whichLabel2 = gridData.text2;
         var offsetX = gridX;
         var offsetY = gridY - 125;
 
         if (whichLabel1) {
+            label1startY = whichLabel1.Y + offsetY;
             $('#labelText1').text(whichLabel1.text);
             $('#labelText1').css('left', (whichLabel1.X + offsetX));
-            $('#labelText1').css('top', (whichLabel1.Y + offsetY));
+            $('#labelText1').css('top', label1startY);
         } else {
             $('#labelText1').text("");
         }
         if (whichLabel2) {
+            label2startY = whichLabel2.Y + offsetY;
             $('#labelText2').text(whichLabel2.text);
             $('#labelText2').css('left', (whichLabel2.X + offsetX));
-            $('#labelText2').css('top', (whichLabel2.Y + offsetY));
+            $('#labelText2').css('top', label2startY);
         } else {
             $('#labelText2').text("");
         }
---
+++ scrollPlugin.js
@@ -0,0 +1,64 @@
+(function(){
+
+    var special = jQuery.event.special,
+        uid1 = 'D' + (+new Date()),
+        uid2 = 'D' + (+new Date() + 1);
+
+    special.scrollstart = {
+        setup: function() {
+
+            var timer,
+                handler =  function(evt) {
+
+                    var _self = this,
+                        _args = arguments;
+
+                    if (timer) {
+                        clearTimeout(timer);
+                    } else {
+                        evt.type = 'scrollstart';
+                        jQuery.event.handle.apply(_self, _args);
+                    }
+
+                    timer = setTimeout( function(){
+                        timer = null;
+                    }, special.scrollstop.latency);
+
+                };
+
+            jQuery(this).bind('scroll', handler).data(uid1, handler);
+
+        },
+        teardown: function(){
+            jQuery(this).unbind( 'scroll', jQuery(this).data(uid1) );
+        }
+    };
+
+    special.scrollstop = {
+        latency: 300,
+        setup: function() {
+            console.log("scrollstop.setup");
+            var timer,
+                    handler = function(evt) {
+                        console.log("handler");
+                        var _self = this,
+                            _args = arguments;
+                        if (timer) {
+                            clearTimeout(timer);
+                        }
+                        timer = setTimeout( function(){
+                            console.log("timer");
+                            timer = null;
+                            evt.type = 'scrollstop';
+                            jQuery.event.handle.apply(_self, _args);
+                        }, special.scrollstop.latency);
+                    };
+            jQuery(this).bind('scroll', handler).data(uid2, handler);
+        },
+        teardown: function() {
+            console.log("scrollstop.teardown");
+            jQuery(this).unbind( 'scroll', jQuery(this).data(uid2) );
+        }
+    };
+
+})();
